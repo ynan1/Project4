9411e3d8ca4e7e36319f57429821deb7ed3e7e95
Yashaswi Nandan <yashaswi646@outlook.com>
2022-12-07 15:54:54 +0530

    1

--- Random generation and their multiplication/Binary random generation_(values 1 -1).cpp
+++ Random generation and their multiplication/Binary random generation_(values 1 -1).cpp
@@ -6,6 +6,7 @@
 int main()
 {
     std::cout << "Thread ID: " << std::thread::hardware_concurrency() << '\n';
+
     int* s = new int[15000] {}; // Here we allocate memory(object) space for hundred integers, an array of hundred
                               // integers using operator new[]. Our pointer s now points at the first
                                 // element in the array. 
@@ -13,11 +14,13 @@ int main()
     int* k = new int[15000] {}; // we can use short int(2 byte) or int(4 byte).
     
 
-    std::cout << "The size of type float is: " << sizeof(float) << " byte(s)" << '\n';
-    std::cout << "The size of type double is: " << sizeof(double) << " byte(s)" << '\n';
+   // std::cout << "The size of type float is: " << sizeof(float) << " byte(s)" << '\n';
+    //std::cout << "The size of type double is: " << sizeof(double) << " byte(s)" << '\n';
 
-    srand(time(0));         // intialising random variable each time program starts, else rand() will give
-    // same value each time program is run.
+    srand(time(0));     // intialising random variable each time program starts, else rand() will give
+    float r = rand();                  // same value each time program is run.
+    std::cout <<"Random initialisation value is: " << r;
+    std::cout << '\n';
 
     for (int i = 0; i < 10000; i++)   // Arrays of 10,000 elements taken here.
     {
