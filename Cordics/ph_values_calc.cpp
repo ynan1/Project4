1847b29b3034a6d11c025db09ae176c30f575ea8
Yashaswi Nandan <yashaswi646@outlook.com>
2023-03-03 18:11:44 +0530

    03.03.23

---
+++ Cordics/ph_values_calc.cpp
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <iostream>
+#include <math.h>
+#include <string>
+
+int main()
+{
+	double pi = 3.141592653589793238463;
+
+	for (int i = 1; i < 13; i++)
+		pi = pi / 2;
+
+}
